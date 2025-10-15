f0e75f73d630ace362653c74b5a44e39b8575c4f
maimohammed928 <maimohammed928@gmail.com>
2025-10-15 13:26:25 +0300

    modify on leaderboard

--- src/components/LeaderboardPopup.jsx
+++ src/components/LeaderboardPopup.jsx
@@ -5,18 +5,30 @@ const LeaderboardPopup = ({ isOpen, onClose, leaderboardData }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
-      <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-lg relative">
-        <h3 className="text-2xl font-semibold mb-4">Course Leaderboard</h3>
+      <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-md relative">
+        <div className='text-[#080264] text-center my-3'>
+          <h3 className="  ">Course Name Shown Here</h3>
+          <p className='font-semibold'>Leaderboard</p>
+        </div>
+        <div className='flex items-center gap-3 px-2 text-[#080264] bg-gray-50'>
+          <p className='text-end text-sm py-2 '>عظيم يا صديقي.. أداءك في الكورس ده أفضل من 60% من باقي الطلبة.. كمّل عايز أشوف اسمك في الليدر بورد هنا
+
+          </p>
+          <img width={30} src='/support.png' />
+
+        </div>
+
         <button
           onClick={onClose}
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-800 text-2xl"
         >
           &times;
         </button>
-        <div className="space-y-3">
+        <div className="space-y-3 mt-7 bg-gray-50 p-4">
           {leaderboardData.length > 0 ? (
             leaderboardData.map((entry, index) => (
-              <div key={index} className="flex items-center justify-between p-3 bg-gray-50 rounded-md">
+              <div key={index} className="flex items-center justify-between p-3 bg-white  rounded-md">
+
                 <span className="font-medium text-gray-800">{index + 1}. {entry.name}</span>
                 <span className="text-blue-600 font-bold">{entry.score} points</span>
               </div>
